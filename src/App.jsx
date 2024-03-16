2f0d2168e5347ee02a8124395395a3533d9f9b24
EidmantasDev <eidmantas.slenys@gmail.com>
2024-03-16 09:22:12 +0200

    Outputting dynamic values

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
+const generateDesctiption = () => {
+  const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
+  const reactDescriptionsSize = reactDescriptions.length;
+  const generateRandomInt = (max) => Math.floor(Math.random() * max);
+  return reactDescriptions[generateRandomInt(reactDescriptionsSize)];
+};
+
 function Header() {
   return (
     <header>
       <img src='src/assets/react-core-concepts.png' alt='Stylized atom' />
       <h1>React Essentials</h1>
       <p>
-        Fundamental React concepts you will need for almost any app you are
-        going to build!
+        {generateDesctiption()} React concepts you will need for almost any app
+        you are going to build!
       </p>
     </header>
   );
