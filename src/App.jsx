5e7525b016bee7b63d3e62b4e6ba3b465ed25cbf
EidmantasDev <eidmantas.slenys@gmail.com>
2024-03-16 09:26:26 +0200

    Optimized outputting dynamic values Refactored image

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
+import reactImg from './assets/react-core-concepts.png';
+
+const generateRandomInt = (max) => Math.floor(Math.random() * max);
+
 const generateDesctiption = () => {
   const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
-  const reactDescriptionsSize = reactDescriptions.length;
-  const generateRandomInt = (max) => Math.floor(Math.random() * max);
-  return reactDescriptions[generateRandomInt(reactDescriptionsSize)];
+  const { length } = reactDescriptions;
+  const randomIndex = generateRandomInt(length);
+  return reactDescriptions[randomIndex];
 };
 
 function Header() {
   return (
     <header>
-      <img src='src/assets/react-core-concepts.png' alt='Stylized atom' />
+      <img src={reactImg} alt='Stylized atom' />
       <h1>React Essentials</h1>
       <p>
         {generateDesctiption()} React concepts you will need for almost any app
