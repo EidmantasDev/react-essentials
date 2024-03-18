72c39db42d3c5ace4a308972cbb2a419ed476461
EidmantasDev <eidmantas.slenys@gmail.com>
2024-03-18 09:40:07 +0200

    Added new components TabContent to display EXAMPLES data from data.js once TabButton component is clicked Init useState to handle TabContent switch Added vite.svg and key to CoreConcept list  to fix all warnings in console

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
-import { CORE_CONCEPTS } from './data';
+import { useState } from 'react';
+import { CORE_CONCEPTS, EXAMPLES } from './data';
 import Header from './components/Header/Header';
 import CoreConcept from './components/CoreConcept/CoreConcept';
+import TabButton from './components/TabButton/TabButton';
+import TabContent from './components/TabContent/TabContent';
 
 function App() {
+  const [selectedTopic, setSelectedTopic] = useState('components');
+
+  function handleSelect(selectedButton) {
+    setSelectedTopic(selectedButton);
+  }
+
   return (
     <div>
       <Header />
@@ -10,10 +19,22 @@ function App() {
         <section id='core-concepts'>
           <h2>Core Concepts</h2>
           <ul>
-            {CORE_CONCEPTS.map((concept) => (
-              <CoreConcept {...concept} />
+            {CORE_CONCEPTS.map((concept, index) => (
+              <CoreConcept key={index} {...concept} />
             ))}
           </ul>
+        </section>
+        <section id='examples'>
+          <h2>Examples</h2>
+          <menu>
+            <TabButton onSelect={() => handleSelect('components')}>
+              Components
+            </TabButton>
+            <TabButton onSelect={() => handleSelect('jsx')}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
+          </menu>
+          {selectedTopic && <TabContent {...EXAMPLES[selectedTopic]} />}
         </section>
       </main>
     </div>
